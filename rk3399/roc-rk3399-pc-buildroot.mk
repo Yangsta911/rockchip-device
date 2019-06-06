dc030f3452b6cd21c6478ddad8bed8574f79cf50
daijh <service@t-firefly.com>
2019-06-06 11:15:30 +0800

    roc-rk3399-pc: modify uboot config
    Change-Id: Ie88a518a4eec5a1feeea47131bf9b8982d5bf33d

--- rk3399/roc-rk3399-pc-buildroot.mk
+++ rk3399/roc-rk3399-pc-buildroot.mk
@@ -5,5 +5,8 @@ CUR_DIR=`dirname $CMD`
 
 source $CUR_DIR/firefly-rk3399-buildroot.mk
 
+# Uboot defconfig
+export RK_UBOOT_DEFCONFIG=roc-rk3399-pc
+
 # Kernel dts
 export RK_KERNEL_DTS=rk3399-roc-pc
--- rk3399/roc-rk3399-pc-ubuntu.mk
+++ rk3399/roc-rk3399-pc-ubuntu.mk
@@ -5,5 +5,9 @@ CUR_DIR=`dirname $CMD`
 
 source $CUR_DIR/firefly-rk3399-ubuntu.mk
 
+# Uboot defconfig
+export RK_UBOOT_DEFCONFIG=roc-rk3399-pc
+
 # Kernel dts
 export RK_KERNEL_DTS=rk3399-roc-pc
+
