a11f7ab0b5833c10ac5d485b896aa7367440e80d
huangjc <service@t-firefly.com>
2019-09-17 16:01:17 +0800

    rk3399: roc-rk3399-pc-plus-ubuntu.mk: modify kernel config

--- rk3399/roc-rk3399-pc-plus-ubuntu.mk
+++ rk3399/roc-rk3399-pc-plus-ubuntu.mk
@@ -6,7 +6,8 @@ CUR_DIR=`dirname $CMD`
 source $CUR_DIR/firefly-rk3399-ubuntu.mk
 
 # Kernel defconfig
-export RK_KERNEL_DEFCONFIG=firefly_roc-rk3399-pc_defconfig
+export RK_KERNEL_DEFCONFIG=firefly_linux_defconfig
+#export RK_KERNEL_DEFCONFIG=firefly_roc-rk3399-pc_defconfig
 
 # Uboot defconfig
 export RK_UBOOT_DEFCONFIG=roc-rk3399-pc
