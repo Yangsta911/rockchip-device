084827beb8f4591aa94b7f3f3d506b2e82c020fe
zouxf <zxf@t-tchip.com.cn>
2020-03-04 19:09:32 +0800

    firefly: fix face-rk3399 mk
    Change-Id: Ie2a07dfcc374b4390e2311427e6c63bc30746a19

--- rk3399/face-rk3399-ubuntu.mk
+++ rk3399/face-rk3399-ubuntu.mk
@@ -3,28 +3,7 @@
 CMD=`realpath $BASH_SOURCE`
 CUR_DIR=`dirname $CMD`
 
-source $CUR_DIR/BoardConfig.mk
+source $CUR_DIR/firefly-rk3399-ubuntu.mk
 
-# Uboot defconfig
-export RK_UBOOT_DEFCONFIG=firefly-rk3399
-# Kernel defconfig
-export RK_KERNEL_DEFCONFIG=firefly_linux_defconfig
 # Kernel dts
 export RK_KERNEL_DTS=rk3399-firefly-face-mipi8
-# parameter for GPT table
-export RK_PARAMETER=parameter-ubuntu.txt
-# packagefile for make update image 
-export RK_PACKAGE_FILE=rk3399-ubuntu-package-file
-
-# Set rootfs type, including ext2 ext4 squashfs
-export RK_ROOTFS_TYPE=ext4
-# rootfs image path
-#export RK_ROOTFS_IMG=ubunturootfs/rk3399_ubuntu18.04_LXDE.img
-export RK_ROOTFS_IMG=ubunturootfs/face-rk3399_ubuntu18.04.img
-
-# Recovery config
-export RK_CFG_RECOVERY=
-#OEM config
-export RK_OEM_DIR=
-#userdata config
-export RK_USERDATA_DIR=
