f35a6608e5e690b729ba4a53b7a0d4d67d4c727d
luojh <service@t-firefly.com>
2020-05-28 14:28:21 +0800

    rk3399:cluster-aio-3399j-ubuntu.mk:modify dts and rootfs

--- rk3399/cluster-aio-3399j-ubuntu.mk
+++ rk3399/cluster-aio-3399j-ubuntu.mk
@@ -6,5 +6,5 @@ CUR_DIR=`dirname $CMD`
 source $CUR_DIR/firefly-rk3399-ubuntu.mk
 
 # Kernel dts
-export RK_KERNEL_DTS=rk3399-firefly-aio-cluster
-export RK_ROOTFS_IMG=ubunturootfs/ubuntu_18.04_20200413-1513_DESKTOP.img
+export RK_KERNEL_DTS=rk3399-firefly-csr2-main
+export RK_ROOTFS_IMG=ubunturootfs/UBUNTU_18.04_CSR2_Main_DESKTOP.img
