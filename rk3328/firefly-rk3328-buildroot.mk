5503328914084b91143e6bcb9a1b881474fe4eb2
liulq <llq@t-chip.com.cn>
2020-06-09 14:20:45 +0800

    firefly: buildroot: use parameter-ubuntu

--- rk3328/firefly-rk3328-buildroot.mk
+++ rk3328/firefly-rk3328-buildroot.mk
@@ -11,10 +11,19 @@ export RK_UBOOT_DEFCONFIG=firefly-rk3328
 export RK_KERNEL_DEFCONFIG=firefly_linux_defconfig
 # Kernel dts
 export RK_KERNEL_DTS=rk3328-roc-cc
+# parameter for GPT table
+export RK_PARAMETER=parameter-ubuntu.txt
 # packagefile for make update image 
-export RK_PACKAGE_FILE=rk3328-package-file
+export RK_PACKAGE_FILE=rk3328-ubuntu-package-file
 
 # sd_parameter for GPT table
 export RK_SD_PARAMETER=parameter-recovery.txt
 # packagefile for make sdupdate image
 export RK_SD_PACKAGE_FILE=rk3328-recovery-package-file
+
+# Set userdata partition type
+export RK_USERDATA_FS_TYPE=ext4
+#OEM config
+export RK_OEM_DIR=
+#userdata config
+export RK_USERDATA_DIR=
--- rk3399/firefly-rk3399-buildroot.mk
+++ rk3399/firefly-rk3399-buildroot.mk
@@ -11,10 +11,19 @@ export RK_UBOOT_DEFCONFIG=firefly-rk3399
 export RK_KERNEL_DEFCONFIG=firefly_linux_defconfig
 # Kernel dts
 export RK_KERNEL_DTS=rk3399-firefly
-# packagefile for make update image 
-export RK_PACKAGE_FILE=rk3399-package-file
+# parameter for GPT table
+export RK_PARAMETER=parameter-ubuntu.txt
+# packagefile for make update image
+export RK_PACKAGE_FILE=rk3399-ubuntu-package-file
 
 # sd_parameter for GPT table
 export RK_SD_PARAMETER=parameter-recovery.txt
 # packagefile for make sdupdate image
 export RK_SD_PACKAGE_FILE=rk3399-recovery-package-file
+
+# Set userdata partition type
+export RK_USERDATA_FS_TYPE=ext4
+#OEM config
+export RK_OEM_DIR=
+#userdata config
+export RK_USERDATA_DIR=
