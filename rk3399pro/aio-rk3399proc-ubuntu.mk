9f656f46fccad265e783c9ed833d03634afad9f8
liulq <llq@t-chip.com.cn>
2020-09-05 17:38:50 +0800

    firefly: set rk3399pro ubuntu use overlay rootfs

--- rk3399pro/aio-rk3399proc-ubuntu.mk
+++ rk3399pro/aio-rk3399proc-ubuntu.mk
@@ -20,7 +20,13 @@ export RK_PACKAGE_FILE=rk3399-ubuntu-package-file
 export RK_ROOTFS_TYPE=ext4
 # rootfs image path
 export RK_ROOTFS_IMG=ubunturootfs/rk3399pro_ubuntu18.04_LXDE.img
+# recovery ramdisk
+export RK_RECOVERY_RAMDISK=recovery-arm64.cpio.gz
+# Set userdata partition type
+export RK_USERDATA_FS_TYPE=ext4
 
+# Buildroot config
+export RK_CFG_BUILDROOT=
 # Recovery config
 export RK_CFG_RECOVERY=
 #OEM config
