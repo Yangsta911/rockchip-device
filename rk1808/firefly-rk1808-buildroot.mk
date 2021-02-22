32b370eacdd555951777870a1f57f803852a9fc8
daijh <djh@t-chip.com.cn>
2021-02-22 10:37:25 +0800

    rk1808: use firefly buildroot config

--- rk1808/firefly-rk1808-buildroot.mk
+++ rk1808/firefly-rk1808-buildroot.mk
@@ -13,5 +13,7 @@ export RK_KERNEL_DEFCONFIG=firefly_rk1808_defconfig
 export RK_KERNEL_DTS=rk1808-firefly
 # packagefile for make update image 
 export RK_PACKAGE_FILE=rk1808-package-file
+# Buildroot config
+export RK_CFG_BUILDROOT=firefly_rk1808
 
 export RK_USERDATA_FS_TYPE=ext4
