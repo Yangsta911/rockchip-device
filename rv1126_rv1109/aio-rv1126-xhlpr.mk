78e2446e424efb6a966fa2c7312d7da689aae294
daijh <djh@t-chip.com.cn>
2021-08-20 15:56:57 +0800

    rv1126_rv1109: xhlpr: fix delete media parameter

--- rv1126_rv1109/aio-rv1126-xhlpr.mk
+++ rv1126_rv1109/aio-rv1126-xhlpr.mk
@@ -10,4 +10,5 @@ source $CUR_DIR/aio-rv1126-jd4.mk
 export RK_CFG_BUILDROOT=firefly_rv1126_rv1109_xhlpr
 # OEM config
 export RK_OEM_DIR=oem_ipc_firefly_xhlpr
-
+# parameter for GPT table
+export RK_PARAMETER=parameter-firefly-debian-fit.txt
