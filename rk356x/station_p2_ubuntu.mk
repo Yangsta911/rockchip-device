6543ccc95ead25244d213a6065092cf9375cb229
liulq <llq@t-chip.com.cn>
2021-08-30 09:36:13 +0800

    rk356x: station: use station_linux_defconfig

--- rk356x/station_p2_ubuntu.mk
+++ rk356x/station_p2_ubuntu.mk
@@ -7,5 +7,7 @@ source $CUR_DIR/firefly-rk3568_ubuntu.mk
 
 # Kernel dts
 export RK_KERNEL_DTS=rk3568-firefly-roc-pc
+# Kernel defconfig
+export RK_KERNEL_DEFCONFIG=station_linux_defconfig
 # recovery ramdisk
 export RK_RECOVERY_RAMDISK=rk356x-recovery-arm64-raw.cpio.gz
