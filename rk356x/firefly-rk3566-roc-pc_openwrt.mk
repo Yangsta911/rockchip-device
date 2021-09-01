4667862d9116f04e5f1ade88bebb402797069c33
huangjf <hjf@t-chip.com.cn>
2021-09-01 14:26:57 +0800

    rk356x: update openwrt mk

--- rk356x/firefly-rk3566-roc-pc_openwrt.mk
+++ rk356x/firefly-rk3566-roc-pc_openwrt.mk
@@ -6,13 +6,13 @@ CUR_DIR=`dirname $CMD`
 source $CUR_DIR/firefly-rk356x_openwrt.mk
 
 # Uboot defconfig
-export RK_UBOOT_DEFCONFIG=firefly-rk3568
+export RK_UBOOT_DEFCONFIG=firefly-rk3566
 # Kernel defconfig
 export RK_KERNEL_DEFCONFIG=station_linux_defconfig
 # Kernel dts
-export RK_KERNEL_DTS=rk3568-firefly-roc-pc
+export RK_KERNEL_DTS=rk3566-firefly-roc-pc
 
 # Openwrt version select
 export RK_OPENWRT_VERSION_SELECT=openwrt
 # Openwrt defconfig
-export RK_OPENWRT_DEFCONFIG=ROC-3568-PC_config
+export RK_OPENWRT_DEFCONFIG=ROC-3566-PC_config
