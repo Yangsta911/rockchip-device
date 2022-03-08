45eafe52ed5d7c2c86be041e43cb635fce10af9c
daijh <djh@t-chip.com.cn>
2022-03-08 11:03:50 +0800

    rk3588: add bootable flag to support extboot

--- rk3588/iboard-rk3588q.mk
+++ rk3588/iboard-rk3588q.mk
@@ -6,5 +6,8 @@ source $CUR_DIR/BoardConfig.mk
 # Kernel defconfig fragment
 export RK_KERNEL_DEFCONFIG_FRAGMENT=firefly-linux.config
 
+# parameter for GPT table
+export RK_PARAMETER=parameter-firefly.txt
+
 # Kernel dts
 export RK_KERNEL_DTS=firefly-iboard-3588
