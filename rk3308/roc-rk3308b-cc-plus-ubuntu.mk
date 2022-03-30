79873c10be5b5e12ae9866e52306690683cc4daa
liulq <llq@t-chip.com.cn>
2022-03-30 15:09:02 +0800

    rk3308: update ubuntu.mk for pack with prepare recovery.img without building it

--- rk3308/roc-rk3308b-cc-plus-ubuntu.mk
+++ rk3308/roc-rk3308b-cc-plus-ubuntu.mk
@@ -17,3 +17,18 @@ export RK_PARAMETER=parameter-64bit-ubuntu.txt
 export RK_PACKAGE_FILE=rk3308-package-file-ubuntu
 # rootfs image path
 export RK_ROOTFS_IMG=rootfs/rk3308-ubuntu_rootfs.img
+# recovery ramdisk
+export RK_RECOVERY_RAMDISK=recovery-arm64.cpio.gz
+# Set userdata partition type
+export RK_USERDATA_FS_TYPE=ext4
+
+# Buildroot config
+export RK_CFG_BUILDROOT=
+# Recovery config
+export RK_CFG_RECOVERY=
+#OEM config
+export RK_OEM_DIR=
+#userdata config
+export RK_USERDATA_DIR=
+# rootfs_system
+export RK_ROOTFS_SYSTEM=ubuntu
