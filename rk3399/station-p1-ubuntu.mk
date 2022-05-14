077a10e6d72497965917ae0b9857c2b56d510d34
weicz <wcz@t-chip.com.cn>
2022-05-14 16:30:05 +0800

    rk3399: rename station-p1-pro-ubuntu.mk to station-p1-ubuntu.mk & modify
    Signed-off-by: weicz <wcz@t-chip.com.cn>

--- rk3399/station-p1-ubuntu.mk
+++ rk3399/station-p1-ubuntu.mk
@@ -8,11 +8,8 @@ source $CUR_DIR/firefly-rk3399-ubuntu.mk
 # Kernel defconfig
 export RK_KERNEL_DEFCONFIG=station_linux_defconfig
 
-# Uboot defconfig
-export RK_UBOOT_DEFCONFIG=roc-rk3399-pc-plus
-
 # Kernel dts
-export RK_KERNEL_DTS=rk3399-roc-pc-pro
+export RK_KERNEL_DTS=rk3399-roc-pc-plus
 
 # PRODUCT MODEL
-export RK_PRODUCT_MODEL=ROC_3399_PC_PRO
+export RK_PRODUCT_MODEL=ROC_3399_PC_PLUS
