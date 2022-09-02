297d963b915b2dc32f29669a7338ea1e74cf1ae4
huangjc <service@t-firefly.com>
2022-09-02 15:48:44 +0800

    common: set FIREFLY_RESIZE_ROOTFS_PARTITION: Whether to resize the rootfs partition
    Set whether to resize the rootfs partition table through environment variables

--- rk3399/BoardConfig-ab-base.mk
+++ rk3399/BoardConfig-ab-base.mk
@@ -2,3 +2,4 @@ export RK_PACKAGE_FILE_AB=rk3399-package-file-ab
 export RK_MISC=blank-misc.img
 export RK_PARAMETER=parameter-ab.txt
 export RK_CFG_RECOVERY=
+export FIREFLY_RESIZE_ROOTFS_PARTITION="disabled"
