69c7b5c6cf397a4bafeae6b6b25dc56f45be470d
linmj <lmj@t-chip.com.cn>
2022-11-01 15:28:14 +0800

    rk3588: separate aio-3588mq from aio-3588q

---
+++ rk3588/aio-3588mq-buildroot.mk
@@ -0,0 +1,36 @@
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+source $CUR_DIR/BoardConfig.mk
+
+# Kernel defconfig fragment
+export RK_KERNEL_DEFCONFIG_FRAGMENT=firefly-linux.config
+
+# Uboot_defconfig_fragment
+export RK_UBOOT_DEFCONFIG_FRAGMENT=firefly-linux.config
+
+# parameter for GPT table
+export RK_PARAMETER=parameter-ubuntu-fit.txt
+
+# Kernel dts
+export RK_KERNEL_DTS=rk3588-firefly-aio-3588mq
+
+# Set userdata partition type
+export RK_USERDATA_FS_TYPE=ext4
+
+# Set extboot
+export FF_EXTBOOT=true
+
+export FF_EXTBOOT_SIZE=256M
+
+# PRODUCT MODEL
+export RK_PRODUCT_MODEL=AIO_3588MQ
+
+# recovery ramdisk
+export RK_RECOVERY_RAMDISK=rk3588-recovery-arm64.cpio.gz
+
+# Recovery config
+export RK_CFG_RECOVERY=
+
+# Buildroot config
+export RK_CFG_BUILDROOT=firefly_rk3588
