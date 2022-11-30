01e5e22aa99152321e92687f2111d25c580bb4ab
lvsx <lvsx@t-chip.com.cn>
2022-11-30 10:14:54 +0800

    rv1126_rv1109: add rootfs package

--- rv1126_rv1109/aio-rv1126-jd4-BE-45-debian.mk
+++ rv1126_rv1109/aio-rv1126-jd4-BE-45-debian.mk
@@ -7,3 +7,15 @@ source $CUR_DIR/aio-rv1126-jd4-BE-45.mk
 
 # parameter for GPT table
 export RK_PARAMETER=parameter-firefly-debian-fit.txt
+
+# Set rootfs type, including ext2 ext4 squashfs
+export RK_ROOTFS_TYPE=ext4
+
+# rootfs image path
+export RK_ROOTFS_IMG=ubuntu_rootfs/rootfs.img
+
+# Buildroot config
+export RK_CFG_BUILDROOT=
+
+# rootfs_system
+export RK_ROOTFS_SYSTEM=ubuntu
