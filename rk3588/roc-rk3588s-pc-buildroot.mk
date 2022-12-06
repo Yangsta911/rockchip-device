09d0f727a8aa9e6821bab9257a41c52e14376687
daijh <djh@t-chip.com.cn>
2022-12-06 11:32:05 +0800

    rk3588: roc-rk3588s-pc dts -> roc-rk3588s-pc-mipi101-M101014-BE45-A1

--- rk3588/roc-rk3588s-pc-buildroot.mk
+++ rk3588/roc-rk3588s-pc-buildroot.mk
@@ -4,7 +4,7 @@ CUR_DIR=`dirname $CMD`
 source $CUR_DIR/itx-3588j-buildroot.mk
 
 # Kernel dts
-export RK_KERNEL_DTS=roc-rk3588s-pc
+export RK_KERNEL_DTS=roc-rk3588s-pc-mipi101-M101014-BE45-A1
 
 # PRODUCT MODEL
 export RK_PRODUCT_MODEL=ROC_RK3588S_PC
