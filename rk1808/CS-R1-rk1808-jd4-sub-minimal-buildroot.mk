d967ec3cded103940b6918cf9974b707c5b42266
lvsx <lvsx@t-chip.com.cn>
2022-12-15 10:39:08 +0800

    rk1808: update CS-R1-rk1808-jd4-sub-minimal-buildroot.mk

--- rk1808/CS-R1-rk1808-jd4-sub-minimal-buildroot.mk
+++ rk1808/CS-R1-rk1808-jd4-sub-minimal-buildroot.mk
@@ -5,5 +5,11 @@ CUR_DIR=`dirname $CMD`
 
 source $CUR_DIR/firefly-rk1808-buildroot.mk
 
+# Kernel defconfig fragment
+export RK_KERNEL_DEFCONFIG_FRAGMENT="firefly-linux-cs-r1.config"
+
 # Kernel dts
 export RK_KERNEL_DTS=rk1808-firefly-srjd4-sub
+
+# Buildroot config
+export RK_CFG_BUILDROOT=firefly_rk1808_cs_r1
