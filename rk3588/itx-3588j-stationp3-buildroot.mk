b29a4dcb5f310d9a4a2936165b71341bbc22ae94
daijh <djh@t-chip.com.cn>
2023-01-04 16:57:11 +0800

    rk3588: add stationp3 mk

---
+++ rk3588/itx-3588j-stationp3-buildroot.mk
@@ -0,0 +1,36 @@
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+source $CUR_DIR/BoardConfig.mk
+
+# Kernel defconfig fragment
+export RK_KERNEL_DEFCONFIG_FRAGMENT="$RK_KERNEL_DEFCONFIG_FRAGMENT firefly-linux.config"
+
+# Uboot_defconfig_fragment
+export RK_UBOOT_DEFCONFIG_FRAGMENT=firefly-linux.config
+
+# parameter for GPT table
+export RK_PARAMETER=parameter-ubuntu-fit.txt
+
+# Kernel dts
+export RK_KERNEL_DTS=rk3588-firefly-itx-3588j-station-mipi101-M101014-BE45-A1
+
+# Set userdata partition type
+export RK_USERDATA_FS_TYPE=ext4
+
+# Set extboot
+export FF_EXTBOOT=true
+
+export FF_EXTBOOT_SIZE=256M
+
+# PRODUCT MODEL
+export RK_PRODUCT_MODEL=ITX_3588J
+
+# recovery ramdisk
+export RK_RECOVERY_RAMDISK=rk3588-recovery-arm64.cpio.gz
+
+# Recovery config
+export RK_CFG_RECOVERY=
+
+# Buildroot config
+export RK_CFG_BUILDROOT=firefly_rk3588
