5c79201ea3caa0a0c572070abb20f94d9a9db059
lvsx <lvsx@t-chip.com.cn>
2023-01-06 09:57:43 +0800

    rk3588: aio-3588sjd4 dts -> aio-3588sjd4-mipi101-M101014-BE45-A1

--- rk3588/aio-3588sjd4-buildroot.mk
+++ rk3588/aio-3588sjd4-buildroot.mk
@@ -4,7 +4,7 @@ CUR_DIR=`dirname $CMD`
 source $CUR_DIR/roc-rk3588s-pc-buildroot.mk
 
 # Kernel dts
-export RK_KERNEL_DTS=aio-3588sjd4
+export RK_KERNEL_DTS=aio-3588sjd4-mipi101-M101014-BE45-A1
 
 # PRODUCT MODEL
 export RK_PRODUCT_MODEL=AIO_3588SJD4
