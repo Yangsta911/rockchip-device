f8f13721a8294b1a48c874f138cb66f9331ecd49
lvsx <lvsx@t-chip.com.cn>
2023-01-10 09:53:55 +0800

    rk3588: compatible: rk3588j,rk3588m and rk3588 can use the same firmware

--- rk3588/aio-3588mq-BE45-A1-ubuntu.mk
+++
@@ -1,7 +0,0 @@
-CMD=`realpath $BASH_SOURCE`
-CUR_DIR=`dirname $CMD`
-
-source $CUR_DIR/aio-3588mq-ubuntu.mk
-
-# Kernel dts
-export RK_KERNEL_DTS=rk3588-firefly-aio-3588mq-mipi101-M101014-BE45-A1
--- rk3588/aio-3588mq-edp-debian.mk
+++
@@ -1,9 +0,0 @@
-CMD=`realpath $BASH_SOURCE`
-CUR_DIR=`dirname $CMD`
-
-DEVICE_NAME=$(echo ${CMD} | awk -F '/' '{print $NF}')
-DEVICE_NAME=${DEVICE_NAME%\-*}
-source $CUR_DIR/${DEVICE_NAME}-ubuntu.mk
-
-# rootfs_system
-export RK_ROOTFS_SYSTEM=debian
