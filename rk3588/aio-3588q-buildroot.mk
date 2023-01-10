1dd506824ed7565698cdd40ef0d96510e9fa2505
lvsx <lvsx@t-chip.com.cn>
2023-01-10 10:00:45 +0800

    rk3588: aio-3588q dts -> rk3588-firefly-aio-3588q-mipi101-M101014-BE45-A1

--- rk3588/aio-3588q-buildroot.mk
+++ rk3588/aio-3588q-buildroot.mk
@@ -4,7 +4,7 @@ CUR_DIR=`dirname $CMD`
 source $CUR_DIR/itx-3588j-buildroot.mk
 
 # Kernel dts
-export RK_KERNEL_DTS=rk3588-firefly-aio-3588q
+export RK_KERNEL_DTS=rk3588-firefly-aio-3588q-mipi101-M101014-BE45-A1
 
 # PRODUCT MODEL
 export RK_PRODUCT_MODEL=AIO_3588Q
