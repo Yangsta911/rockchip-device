d84719b9f565c413f3bb2b9eac4b2b6e54cefb2e
linmj <lmj@t-chip.com.cn>
2023-07-04 16:19:10 +0800

    add rk356x/aio-3568gw-ubuntu.mk

---
+++ rk356x/aio-3568gw-ubuntu.mk
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+source $CUR_DIR/firefly-rk3568-ubuntu.mk
+
+# Kernel dts
+export RK_KERNEL_DTS=rk3568-firefly-aio-3568gw
+# PRODUCT MODEL
+export RK_PRODUCT_MODEL=AIO-3568GW
