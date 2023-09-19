5161b060d141b9ae5b6e455e215d14ff37cb9ee1
dengkx <dkx@t-chip.com.cn>
2023-09-19 16:22:48 +0800

    rk3588: Add roc-rk3588-pc-se

---
+++ rk3588/roc-rk3588-pc-se-buildroot.mk
@@ -0,0 +1,10 @@
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+source $CUR_DIR/itx-3588j-buildroot.mk
+
+# Kernel dts
+export RK_KERNEL_DTS=roc-rk3588-pc-se
+
+# PRODUCT MODEL
+export RK_PRODUCT_MODEL=ROC_RK3588_PC_SE
