d6ba0aea68e9a2c377fd3ff40cdffdbc1aa84b7a
zhangzz <zzz@t-chip.com.cn>
2023-10-23 17:29:18 +0800

    rk3588: Add aio-3588l

---
+++ rk3588/aio-3588l-buildroot.mk
@@ -0,0 +1,10 @@
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+source $CUR_DIR/itx-3588j-buildroot.mk
+
+# Kernel dts
+export RK_KERNEL_DTS=aio-3588l-mipi101-M101014-BE45-A1
+
+# PRODUCT MODEL
+export RK_PRODUCT_MODEL=AIO-3588L
