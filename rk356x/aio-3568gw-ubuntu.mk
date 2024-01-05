92b16f0ffdb153633aaa7e48b4ccce69961a683f
maocl <mcl@t-chip.com.cn>
2024-01-05 10:48:30 +0800

    rk3568/firefly.json : add product AIO_3568GW

--- rk356x/aio-3568gw-ubuntu.mk
+++ rk356x/aio-3568gw-ubuntu.mk
@@ -8,4 +8,4 @@ source $CUR_DIR/firefly-rk3568-ubuntu.mk
 # Kernel dts
 export RK_KERNEL_DTS=rk3568-firefly-aio-3568gw
 # PRODUCT MODEL
-export RK_PRODUCT_MODEL=AIO-3568GW
+export RK_PRODUCT_MODEL=AIO_3568GW
