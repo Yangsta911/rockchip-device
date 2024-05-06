f91a4d13d82341272bab9e19a7aa3c96ce6d4588
Yangsta911 <yangxin.sz.ch@gmail.com>
2024-05-06 17:51:41 +1000

    fix(firefly_rk3568_fix): rk3568 ethernet error temp fix

--- rk356x/firefly-rk3566-buildroot.mk
+++ rk356x/firefly-rk3566-buildroot.mk
@@ -9,6 +9,8 @@ source $CUR_DIR/BoardConfig.mk
 export RK_UBOOT_DEFCONFIG=firefly-rk3566
 # Kernel defconfig
 export RK_KERNEL_DEFCONFIG=firefly_linux_defconfig
+# Kernel dts
+export RK_KERNEL_DTS=rk3568-firefly-roc-pc
 # packagefile for make update image 
 export RK_PACKAGE_FILE=rk356x-package-file
 # build idblock
